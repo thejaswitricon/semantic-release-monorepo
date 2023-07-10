97446a29f0f720bf8db995c182bc5250bcc263d4
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-10 17:02:50 +0530

    fix(root): initial

---
+++ release.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    branch: 'master',
+    monorepo: {
+      analyzeCommits: [
+        '@semantic-release/commit-analyzer'
+      ],
+      generateNotes: [
+        '@semantic-release/release-notes-generator'
+      ]
+    },
+    verifyConditions: [],
+    verifyRelease: [],
+    prepare: [],
+    publish: [],
+    success: [
+      '@semantic-release/github'
+    ],
+    fail: [
+      '@semantic-release/github'
+    ],
+    generateNotes: [
+      {
+        path: '@semantic-release/changelog',
+        changelogTitle: '# CHANGELOG',
+        changelogFile: 'CHANGELOG.md',
+        changelogCompareUrlFormat: '${repoUrl}/compare/${previousTag}...${currentTag}',
+      }
+    ],
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateVersion: [],
+    verifyConditions: [],
+    getLastRelease: [],
+    publish: [],
+    success: [],
+    fail: []
+  };
+  
